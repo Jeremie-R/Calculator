0f15fa6b74c0b245ea7c5ac5e78d861f738df434
farah <140409352+Jeremie-R@users.noreply.github.com>
2023-12-21 15:13:26 +0100

    adding theme change

--- app.js
+++ app.js
@@ -1,10 +1,65 @@
 // app
-
+let currentThemeValue = "string";
 
 
 // on load
 window.onload = function () {
     
-    console.log("page loaded");
+    //colour mode
+    if (window.matchMedia) {
+        if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+          // Dark
+            setTheme("dark");
+        } else {
+          // Light
+            setTheme("ligh");
+        }
+      } else {
+        // Default 
+        setTheme("ligh");
+      }
     
 };
+
+
+//Listen to user changing theme
+
+let themeSwitch = document.getElementById("themeSwitch");
+themeSwitch.addEventListener("click", changeTheme);
+
+function changeTheme() {
+    //if one change to the other
+    if (currentThemeValue === "dark") {
+        setTheme("light");
+    } else {
+        setTheme("dark");
+    }
+}
+
+
+
+//change  theme
+
+function setTheme(theme) {
+
+    let themeSwitch = document.getElementById("themeSwitch");
+    let themeCSS = document.getElementById("themeCSS");
+    let logo = document.getElementById("logo");
+
+    console.log("loading theme: " + theme)
+
+    if (theme === "dark") {
+        themeCSS.setAttribute('href', './styles/dark.css')
+        themeSwitch.checked = false;
+        currentThemeValue = "dark";
+        logo.setAttribute('src', './assets/logoDark.svg')
+
+    } else {
+        //light
+        themeCSS.setAttribute('href', './styles/light.css')
+        themeSwitch.checked = true;
+        currentThemeValue = "light";
+        logo.setAttribute('src', './assets/logo.svg')
+    }
+
+}
